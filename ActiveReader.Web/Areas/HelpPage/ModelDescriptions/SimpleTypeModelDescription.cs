c55531ca12ffe2b2ae5a96eb03db9e29cf3d409e
Denis Nekrylov <neodenit@gmail.com>
2016-02-06 17:34:16 +0300

    Add default SPA project

---
+++ ActiveReader.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ActiveReader.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ActiveReader.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ActiveReader.Web.MvcApplication" Language="C#" %>
