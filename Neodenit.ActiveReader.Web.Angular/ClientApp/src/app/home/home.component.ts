a61115c953f8953a047c2d7760e7f2d6ebc004bc
Denis Nekrylov <neodenit@gmail.com>
2019-12-26 22:48:11 +0200

    Add authentication check for articles list displaying

--- Neodenit.ActiveReader.Web.Angular/ClientApp/src/app/home/home.component.ts
+++ Neodenit.ActiveReader.Web.Angular/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthorizeService } from '../../api-authorization/authorize.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public isAuthenticated: Observable<boolean>;
+
+  constructor(private authorizeService: AuthorizeService) { }
+
+  ngOnInit() {
+    this.isAuthenticated = this.authorizeService.isAuthenticated();
+  }
 }
