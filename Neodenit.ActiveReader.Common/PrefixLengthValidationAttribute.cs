b974aa6635a94ad9d9fcfca96f930db19370037a
Denis Nekrylov <neodenit@gmail.com>
2020-02-10 18:25:09 +0200

    Add server side validation

---
+++ Neodenit.ActiveReader.Common/PrefixLengthValidationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Neodenit.ActiveReader.Common
+{
+    public class PrefixLengthValidationAttribute: ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var prefixLength = (int)value;
+
+            var minPrefixLength = 1;
+            var maxPrefixLength = CoreSettings.Default.PrefixLength;
+
+            var isValid = minPrefixLength <= prefixLength && prefixLength <= maxPrefixLength;
+            return isValid;
+        }
+    }
+}
--- Neodenit.ActiveReader.Common/ViewModels/ArticleViewModel.cs
+++ Neodenit.ActiveReader.Common/ViewModels/ArticleViewModel.cs
@@ -1,13 +1,18 @@
-﻿namespace Neodenit.ActiveReader.Common.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Neodenit.ActiveReader.Common.ViewModels
 {
     public class ArticleViewModel
     {
         public int? ID { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Text { get; set; }
 
+        [PrefixLengthValidation]
         public int PrefixLength { get; set; }
     }
 }
