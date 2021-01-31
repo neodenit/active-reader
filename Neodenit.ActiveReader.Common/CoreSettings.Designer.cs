fa4d0a967c12577a88fd18e389f8cf8833a06177
Denis Nekrylov <neodenit@gmail.com>
2021-01-31 20:16:04 +0200

    Enable three word answer option

--- Neodenit.ActiveReader.Common/CoreSettings.Designer.cs
+++ Neodenit.ActiveReader.Common/CoreSettings.Designer.cs
@@ -12,7 +12,7 @@ namespace Neodenit.ActiveReader.Common {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     public sealed partial class CoreSettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static CoreSettings defaultInstance = ((CoreSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new CoreSettings())));
@@ -115,7 +115,7 @@ namespace Neodenit.ActiveReader.Common {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("3")]
         public int AnswerLengthMaxOption {
             get {
                 return ((int)(this["AnswerLengthMaxOption"]));
